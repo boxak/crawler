65313f7813b1c2471f93528beb1e6e54f6b47102
boxak <boxak@naver.com>
2022-01-10 20:30:34 +0900

    2022-01-10 maven.yml 삭제

--- src/main/java/com/javas/crawler/CallController.java
+++ src/main/java/com/javas/crawler/CallController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CallController {
-    // github action build test
     @GetMapping("/test")
     public String test() {
         return "project working!!!!";
