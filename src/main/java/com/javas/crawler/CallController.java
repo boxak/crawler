a1962e03a9bc06f2c6b7f6863cd3c2eda672b19f
boxak <boxak@naver.com>
2022-01-10 20:22:50 +0900

    2022-01-10 github action build test

--- src/main/java/com/javas/crawler/CallController.java
+++ src/main/java/com/javas/crawler/CallController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CallController {
+    // github action build test
     @GetMapping("/test")
     public String test() {
         return "project working!!!!";
